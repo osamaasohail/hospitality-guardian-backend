cdca5aed51a556cbf30b12492f446887fd6bf56b
osamaasohail <128741709+osamaasohail@users.noreply.github.com>
2023-05-08 23:49:12 +0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
 require("dotenv").config();
-var url = " mongodb://127.0.0.1:27020/hc";//process.env.MONGODB_URL;
+var url = "mongodb://127.0.0.1:27020/hc";//process.env.MONGODB_URL;
 console.log("mongo url:",url);
 module.exports = url;
