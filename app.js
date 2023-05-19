c59c4581db88ce07062ce5c99b16e61063322f8c
YasIngenetic <113696047+YasIngenetic@users.noreply.github.com>
2023-05-19 21:16:20 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 var createError = require('http-errors');
+const fs = require('fs');
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 var configDB = require('./src/config/db');
+const https = require('https');
+// Certificate
+const privateKey = fs.readFileSync('/home/ubuntu/proj/backend/private.key', 'utf8');
+const certificate = fs.readFileSync('/home/ubuntu/proj/backend/certificate.crt', 'utf8');
+const ca = fs.readFileSync('/home/ubuntu/proj/backend/ca_bundle.crt', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 app.use(logger('dev'));
 app.use(express.json());
 const port = process.env.PORT || '4000';
 app.use(express.urlencoded({ extended: false }));
 mongoose.connect(configDB);
 app.use(cors());
+app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
@@ -34,4 +47,8 @@ app.use((req, res, next) => {
 });
 app.listen(port, function() {
     console.log("Listening on port "+ port);
+});
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(3000, () => {
+	console.log('HTTPS Server running on port 443');
 });
